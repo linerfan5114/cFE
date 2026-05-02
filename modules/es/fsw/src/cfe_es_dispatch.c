e70af33fa95d55d6f428d85e30925107a325ba1c
erfan_mohammadi <linerfan1378304594@gmail.com>
2026-05-02 22:08:53 +0330

    Fix #986: Add length validation for CFE_ES_SEND_HK_MID in TaskPipe

--- modules/es/fsw/src/cfe_es_dispatch.c
+++ modules/es/fsw/src/cfe_es_dispatch.c
@@ -285,7 +285,10 @@ void CFE_ES_TaskPipe(const CFE_SB_Buffer_t *SBBufPtr)
         /*
         ** Housekeeping telemetry request
         */
-        CFE_ES_SendHkCmd((const CFE_ES_SendHkCmd_t *)SBBufPtr);
+        if (CFE_ES_VerifyCmdLength(&SBBufPtr->Msg, sizeof(CFE_ES_SendHkCmd_t)))
+        {
+            CFE_ES_SendHkCmd((const CFE_ES_SendHkCmd_t *)SBBufPtr);
+        }
     }
     else if (CFE_SB_MsgId_Equal(MessageID, CMD_MID))
     {
